56d25404854e6b955fc5755b5902f96833d74608
Ismail Opatola <opatolamails@gmail.com>
2019-07-05 10:21:00 +0100

    include rules for less

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,12 @@ module.exports = {
   devServer: { contentBase: './dist' },
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: ['babel-loader', 'eslint-loader'] },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+      { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
     ],
   },
 };
