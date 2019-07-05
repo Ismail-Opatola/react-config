bd63edfbaeae3c9839268993d879b3dba90729eb
Ismail Opatola <opatolamails@gmail.com>
2019-07-05 10:31:41 +0100

    update your webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   devtool: 'inline-source-map',
   entry: './src/index.js',
@@ -13,4 +15,5 @@ module.exports = {
       { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
     ],
   },
+  plugins: [    new HtmlWebpackPlugin({      template: path.resolve('./index.html'),    }),]
 };
